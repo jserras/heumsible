21e7513745900d1fbea1386c3974bc064249f30a
pmouthaan <pmouthaan@voxbone.com>
2018-04-28 11:45:41 +0200

    dirjax it is

--- prep.sh
+++ prep.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get dist-upgrade
 apt-get install python sudo openssh-server
-#adduser pmouthaan sudo
-echo "pmouthaan ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/pmouthaan
-sudo chmod 440 /etc/sudoers.d/pmouthaan
+#adduser dirjax sudo
+echo "dirjax ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/dirjax
+sudo chmod 440 /etc/sudoers.d/dirjax
