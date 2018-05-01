4b606d310fa8236af24aba3a8cba32f854373f48
pmouthaan <pmouthaan@voxbone.com>
2018-05-01 16:34:46 +0200

    config, config, config

--- roles/desktop/files/.bash_workspaces.sh
+++ roles/desktop/files/.bash_workspaces.sh
@@ -9,7 +9,7 @@ done
 function workspace(){
 
   ### make sure workspaces don't mix
-  for j in r m v; do
+  for j in r m; do
     for i in $(alias |grep -Po " ${j}-.*?=") ; do
         unalias $(echo ${i} | sed "s/.$//");
     done;
@@ -27,14 +27,6 @@ function workspace(){
     alias m-$(basename ${m}| cut -d . -f 1 )="cd $1/.mutt; mutt -F ${m}; cd -; clear";
   done
 
-  ### virtualenv aliases
-  for v in $(find $1/.venv/ -mindepth 1 -maxdepth 1 2>/dev/null);
-  do
-    if [ -f ${v}/bin/activate ]; then
-      alias v-$(basename ${v})="source ${v}/bin/activate ; if [[ $(basename ${v}) =~ ansible.* ]]; then ansible --version ;fi"
-    fi
-  done
-
   ### lets move to the workspace
   cd $1
 
@@ -43,18 +35,20 @@ function workspace(){
   export ANSIBLE_VAULT_PASSWORD_FILE="$1/.ansible_vault.txt"
 
   alias gdrive="google-drive-ocamlfuse -label $(basename ${1}) $1/gdrive/"
-  ### some specialties
-  if [[ "$(basename $1)" == "self" ]]; then
-    alias tg="cd $1/.venv/tg/tg; ./bin/telegram-cli; cd -"
-    alias twat="source $1/.venv/twat/bin/activate; pip install -U rainbowstream ; rainbowstream"
-    alias n-music="sudo mount.cifs //noctrifius.at.heum/music /home/pmouthaan/Documents/self/mounts/music/  -ouid=pmouthaan,gid=pmouthaan,user=dirjax"
-    alias n-personal="sudo mount.cifs //noctrifius.at.heum/personal /home/pmouthaan/Documents/self/mounts/personal/  -ouid=pmouthaan,gid=pmouthaan,user=dirjax"
-    alias n-film="sudo mount.cifs //noctrifius.at.heum/film /home/pmouthaan/Documents/self/mounts/film/  -ouid=pmouthaan,gid=pmouthaan,user=dirjax"
-  fi
 
 }
+
+### virtualenv aliases
+for v in $(find ~/.venv/ -mindepth 1 -maxdepth 1 2>/dev/null);
+do
+  if [ -f ${v}/bin/activate ]; then
+    alias v-$(basename ${v})="source ${v}/bin/activate ; if [[ $(basename ${v}) =~ ansible.* ]]; then ansible --version ;fi"
+  fi
+done
+
+alias tg="~/Documents/self/tg/bin/telegram-cli"
+alias twat="source ~/.venv/twat/bin/activate; pip install -U rainbowstream ; rainbowstream"
 
 export -f workspace
 ### and add a default
 w-voxbone
-
