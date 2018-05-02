755f4a67a2a81581388b52108149acb85bd49130
pmouthaan <pmouthaan@voxbone.com>
2018-05-02 05:44:54 +0200

    varia

--- prep.sh
+++ prep.sh
@@ -1,5 +1,2 @@
 apt-get update && apt-get dist-upgrade
 apt-get install python sudo openssh-server
-#adduser dirjax sudo
-echo "dirjax ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/dirjax
-sudo chmod 440 /etc/sudoers.d/dirjax
