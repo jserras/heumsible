a7a84d361e36fbad815db7312fdc5d479c273cfe
pmouthaan <pmouthaan@voxbone.com>
2018-05-08 07:35:57 +0200

    fixed

--- roles/desktop/files/bin/setscreens.sh
+++ roles/desktop/files/bin/setscreens.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# build virtual width
-VWIDTH=0;
-for i in $(xrandr -q | grep '[ \*]+'| grep -v "+.Sync" | awk '{print $1}' | cut -d 'x' -f 1); do
-  VWIDTH=$((VWIDTH + i))
-done
-# echo "$VWIDTH"
-
-# get maximal height
-VHEIGHT=0;
-for i in $(xrandr -q | grep '[ \*]+'| grep -v "+.Sync" | awk '{print $1}' | cut -d 'x' -f 2); do
-  if [ "$VHEIGHT" -lt "$i" ] ; then
-    VHEIGHT=$i
-  fi
-done
-# echo "$VHEIGHT"
-
 # build screenlist
 SLIST=""
 SLAST="eDP-1"
@@ -29,7 +13,10 @@ for i in $(xrandr -q |grep -o "^.* connected"  |awk '{print $1}'| sort); do
   fi
 done
 
+for i in $(xrandr -q |grep -o "^.* disconnected"  |awk '{print $1}'| sort); do
+  SLIST="$SLIST --output $i --off"
+done
 
-# echo "/usr/bin/xrandr --fb ${VWIDTH}x${VHEIGHT} $SLIST 2>/dev/null"
-/usr/bin/xrandr --fb ${VWIDTH}x${VHEIGHT} $SLIST 2>/dev/null
+# echo "/usr/bin/xrandr  $SLIST 2>/dev/null"
+/usr/bin/xrandr  $SLIST 2>/dev/null
 
